61722049c5d5e8f3e9355afc38661f5a232a2f56
Ignacio Molina <molinagranda@gmail.com>
2025-08-03 15:53:57 -0300

    Commit Inicial

---
+++ src/test/java/com/captchaexample/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.captchaexample.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
